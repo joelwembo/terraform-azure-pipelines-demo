d31281d8dc0babb3e48e8571b0321553d7745ecf
Joel Wembo <joelwembo@outlook.com>
2024-02-12 00:22:44 +0800

    updated project structure

---
+++ terraform/s3/provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.70.0"
+    }
+  }
+
+
+  backend "remote" {  
+    hostname="app.terraform.io"  
+    token = "kbrAiDdnwCKEYg.atlasv1.9COhLimGMbbyuQVLFk2ZT7ezuIZ63UfaXl6DsUCTtD9ISmWLHnDk02Vt68DcACPY7W4"
+    organization = "prodxcloud" 
+    workspaces {
+      prefix = "prodxcloud-" 
+    }
+  }
+
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
